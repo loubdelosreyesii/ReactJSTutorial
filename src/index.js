71c057aee94bb0504afe5935f68caebe8f96439a
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-14 15:02:52 +0800

    Lesson 02: Using CreateElement Method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 //Lesson 01: Creating a Component.
 //React component should start with Capital Letter.
 //Creating a component, think of creating a function.
-function Greeting(){
-    return <h2>My First Component</h2>;
+function Greeting2() {
+  return (
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Lesson 02: Implementation of returning an element using the
+//createElement method.
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'Hello World')
+  )
+}
+export default Greeting
 
-root.render(<Greeting/>);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Greeting />)
