a56265c048b812d514a83c57993b1b473e351967
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-14 15:11:18 +0800

    Lesson 03: Using React.Fragment or other semantic in returning an Element.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import ReactDOM from 'react-dom/client'
 //Lesson 01: Creating a Component.
 //React component should start with Capital Letter.
 //Creating a component, think of creating a function.
+//IMPORTANT NOTE: RETURN ONLY 1 PARENT ELEMENT
 function Greeting2() {
   return (
-    <div>
-      <h2>My First Component</h2>
-    </div>
+    // <div>
+    //   <h2>My First Component</h2>
+    // </div>
+    <React.Fragment>
+      <div>
+        <h2>Hello People</h2>
+        <ul>
+          <li>
+            <a href="#">Hello world</a>
+          </li>
+          <li>
+            <a href="#">Hello world!!</a>
+          </li>
+        </ul>
+      </div>
+      <h2>Hello world</h2>
+    </React.Fragment>
   )
 }
 
@@ -25,4 +40,4 @@ export default Greeting
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<Greeting2 />)
