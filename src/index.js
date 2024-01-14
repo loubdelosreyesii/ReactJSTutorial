87f44e6854b5905b9c2f54671a4d3189701469eb
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-14 09:48:51 +0800

    Lesson 01 : Creating a Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-export default Greeting
+import React from 'react';
+import ReactDOM from "react-dom/client";
 
+//Lesson 01: Creating a Component.
+//React component should start with Capital Letter.
+//Creating a component, think of creating a function.
 function Greeting(){
     return <h2>My First Component</h2>;
 }
 
-const Greeting = () =>{
-    return <h2>My First Component</h2>
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting/>);
