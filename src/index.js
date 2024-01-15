06c5f6256dd5fa9fd4c0afeda3ea8098a5f39591
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-15 20:18:34 +0800

    Lesson 03: Return nested components

--- src/index.js
+++ src/index.js
@@ -7,36 +7,17 @@ import ReactDOM from 'react-dom/client'
 //IMPORTANT NOTE: RETURN ONLY 1 PARENT ELEMENT
 function Greeting2() {
   return (
-    // <div>
-    //   <h2>My First Component</h2>
-    // </div>
     <React.Fragment>
-      <div>
-        <h2>Hello People</h2>
-        <ul>
-          <li>
-            <a href="#">Hello world</a>
-          </li>
-          <li>
-            <a href="#">Hello world!!</a>
-          </li>
-        </ul>
-      </div>
-      <h2>Hello world</h2>
+      <Person />
+      <Message />
     </React.Fragment>
   )
 }
 
-//Lesson 02: Implementation of returning an element using the
-//createElement method.
-const Greeting = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h2', {}, 'Hello World')
-  )
+const Person = () => <h2>Loucas II</h2>
+const Message = () => {
+  return <p>This is my message</p>
 }
-export default Greeting
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
