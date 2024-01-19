8d0820cc14edac816ea079b5da4ef7f0401ec126
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-20 05:17:03 +0800

    Lesson 10: Add key to the element

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const books = [
     author: 'Jordan Moore',
     title: 'Interesting Facts for Curious Minds',
     img: './images/book-1.jpg',
+    id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg',
+    id: 2,
   },
 ]
 
@@ -22,8 +24,8 @@ const BookList = () => {
       {books.map((book) => {
         console.log(book)
 
-        const { img, title, author } = book
-        return <Book img={img} title={title} author={author} />
+        const { img, title, author, id } = book
+        return <Book img={img} title={title} author={author} key={id} />
       })}
     </section>
   )
