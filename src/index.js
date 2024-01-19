1e08ac37ce7e7f666effc9511019230ce3c6ffeb
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-01-20 05:12:09 +0800

    Lesson 09: Refactoring the code structure

--- src/index.js
+++ src/index.js
@@ -3,41 +3,28 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts for Curious Minds',
-  img: './images/book-1.jpg',
-}
-
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg',
-}
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts for Curious Minds',
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg',
+  },
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-          laborum veritatis, magnam aut cum fuga ea esse in consequatur
-          dignissimos tempore a quidem optio repudiandae architecto ipsum
-          eveniet voluptatum ratione.
-        </p>
-        <button>Buy Me!</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-      {/* If the property is not assigned with a value, the will use the alternative value assigned. */}
-      {/* <Book author={author} title={title} /> */}
+      {books.map((book) => {
+        console.log(book)
+
+        const { img, title, author } = book
+        return <Book img={img} title={title} author={author} />
+      })}
     </section>
   )
 }
